ed912738ba7e4fa40fd36311358c245c56485ee8
Floris van Maldegem <floris.vanmaldegem@nl.team.blue>
2022-12-02 10:19:14 +0100

    Day 1

---
+++ Day_1/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace AocDay1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = System.IO.File.ReadAllLines("./input");
+
+            Console.WriteLine($"Day 1: {Solve1(input)}");
+            Console.WriteLine($"Day 2: {Solve2(input)}");
+        }
+
+        static string Solve1(string[] input)
+        {
+            int sum = 0;
+            int highest = 0;
+
+            foreach (string line in input)
+            {
+                if(line == "") {
+                    if (sum > highest) highest = sum;
+                    sum = 0;
+                    continue;
+                }
+
+                sum += int.Parse(line);
+            }
+
+            return highest.ToString();
+        }
+
+        static string Solve2(string[] input)
+        {
+            int sum = 0;
+            List<int> topThree = new List<int>() { 0, 0, 0 };
+
+            foreach (string line in input) {
+                if(line == "")
+                {
+                    // Sort the list, so the lowest value will always be on index 0
+                    topThree.Sort();
+                    
+                    if (topThree[0] < sum) topThree[0] = sum;
+                    sum = 0; continue;
+                }
+
+                sum += int.Parse(line);
+            }
+
+            return topThree.Sum().ToString();
+        }
+    }
+}
